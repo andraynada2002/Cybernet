21fb1f10640eb491e658e0b8917d60ad45b8e6f5
andraynada2002 <48077177+andraynada2002@users.noreply.github.com>
2019-04-05 15:46:12 +1000

    Add files via upload

---
+++ cybernet.cs
@@ -0,0 +1,117 @@
+string Filename = @"cybernet.cs"
+class cybernet;
+public class cybernet;
+{
+	//Cybernet type = Net
+	//Cybernet type = Wi-Fi 
+	//Cybernet type = Ethernet
+	//Cybernet type = Optical Vacuolar Net
+	//Cybernet type = DNS
+	//Cybernet DNS IPV4 = 1.1.1.1
+	//Cybernet DNS IPV4 alternate = 1.0.0.1
+	//Cybernet DNS IPV6 =  2606:4700:4700::1111
+	//Cybernet modification IPV6 = 2606:4700:4700::1001
+	//Cybernet type = IPV4
+    //Cybernet type = IPV6	
+	//Cybernet speed = 50gbps
+	//Cybernet speed = 50gb/s
+	//Cybernet connection = optimized
+	//Cybernet connection = fast
+	//Cybernet connection = ultra fast
+	//Cybernet ping = stabled
+	//Cybernet connection = stabled
+	//Cybernet ping = low
+	//Cybernet connection = cyber fast
+	//Cybernet speed = cyber fast
+	//Cybernet ping = cyber low
+	//Cybernet repaired
+	//Cybernet repaired speed
+	//Cybernet repaired ping
+	//Cybernet repaired connection
+	//Cybernet repaired optimization
+	//Cybernet repaired performance
+	//Cybernet repaired svchost
+	//Cybernet repaired adapter
+	//Cybernet repaired load
+	//Cybernet repaired DNS
+	//Cybernet repaired TCP
+	//Cybernet repaired IPV4
+	//Cybernet repaired IPV6
+	//Cybernet repaired IP
+	//Cybernet repaired security
+	//Cybernet repaired antivirus
+	//Cybernet repaired bandwidth
+	//Cybernet type = bandwidth
+	//Cybernet type = QOS
+	//Cybernet repaired QOS
+	//Cybernet repaired Wi-Fi signal
+	//Cybernet repaired Wi-Fi speed
+	//Cybernet repaired Wi-Fi ping
+	//Cybernet repaired Wi-Fi connection
+	//Cybernet repaired Ethernet speed
+	//Cybernet repaired Ethernet ping
+	//Cybernet repaired Ethernet connection
+	//Cybernet type = World Wide Web
+	//Cybernet repaired World Wide Web
+	//Cybernet type =  HyperText Transfer Protocol Secure
+	//Cybernet repaired HyperText Transfer Protocol Secure
+	//Cybernet type = HyperText Transfer Protocol
+	//Cybernet repaired HyperText Transfer Protocol
+	//Cybernet type = HTTP
+	//Cybernet type = HTTPS
+	//Cybernet repaired HTTP
+	//Cybernet repaired HTTPS
+	//Cybernet repaired Rostelecom provider
+	//Cybernet repaired provider
+	//Cybernet repaired random provider
+	//Cybernet repaired Google Chrome
+	//Cybernet repaired Google Chrome performance
+	//Cybernet repaired Internet Explorer
+	//Cybernet repaired Internet Explorer performance
+	//Cybernet repaired Yandex Browser
+	//Cybernet repaired Yandex Browser performance
+	//Cybernet repaired Protocol Secure
+	//Cybernet repaired Protocol
+	//Cybernet repaired servers
+	//Cybernet repaired DNS servers
+	//Cybernet repaired DNS Protocol Secure
+	//Cybernet repaired Data Transfer
+	//Cybernet type Data Transfer
+	//Cybernet type = 4G
+	//Cybernet type = 5G
+	//Cybernet repaired 4G
+	//Cybernet repaired 5G
+	//Cybernet repaired Wi-Fi
+	//Cybernet repaired Ethernet
+    //Cybernet type = Info Net
+	//Cybernet repaired Info Net
+	//Cybernet repaired noise
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
